52afdea954b95218c9563b901125bc322a656d02
Mail3ee <jate2521@gmail.com>
2021-07-05 10:20:53 +0700

    CharacterCard"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import CharacterCard from './CharacterCard';
 
 function App() {
-    return ( <
-        div className = "App" >
-        Hello World <
-        /div>
+    return ( 
+        < div >
+           <CharacterCard/>
+           <CharacterCard/>
+           <CharacterCard/>
+           <CharacterCard/>
+        </div>
     );
 }
 
